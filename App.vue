d34fb1106fb8f005b6741cdc9d43caa005afb1e5
Enzo1994 <120831767@qq.com>
2019-08-11 22:27:07 +0800

    新增：静态样式完成

---
+++ App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card-cardstack">
+    <SingleCard
+      v-for="(item,index) in allCardsArr"
+      :card="item"
+      :key="index"
+      :style="{'top':'10'*index+30+'px','width':(1-0.1*index)*100+'%','z-index':-1000-index*10}"
+    ></SingleCard>
+  </div>
+</template>
+<script>
+import SingleCard from "./SingleCard";
+export default {
+  data() {
+    return {
+      allCardsArr: [{ id: "card1" }, { id: "card1" }, { id: "card1" }]
+    };
+  },
+  components: {
+    SingleCard
+  }
+};
+</script>
+<style scoped>
+.card-cardstack {
+  position: relative;
+  margin: 0 auto;
+  width: 300px;
+  height: 400px;
+  transform: translateY(30%);
+}
+</style>
---
+++ SingleCard.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="card-singlecard">
+    <h1>{{card.id}}</h1>
+  </div>
+</template>
+
+<script>
+import interactjs from "interactjs";
+export default {
+  props: {
+    card: {
+      type: Object,
+      required: true
+    }
+  },
+  mounted() {}
+};
+</script>
+<style scoped>
+.card-singlecard {
+  position: absolute;
+  width: 80%;
+  height: 80%;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: #4158d0;
+  background-image: linear-gradient(
+    43deg,
+    #4158d0 0%,
+    #c850c0 46%,
+    #ffcc70 100%
+  );
+  background-image: -webkit-linear-gradient(
+    43deg,
+    #4158d0 0%,
+    #c850c0 46%,
+    #ffcc70 100%
+  );
+  background-image: -moz-linear-gradient(
+    43deg,
+    #4158d0 0%,
+    #c850c0 46%,
+    #ffcc70 100%
+  );
+  background-image: -o-linear-gradient(
+    43deg,
+    #4158d0 0%,
+    #c850c0 46%,
+    #ffcc70 100%
+  );
+  border-radius: 8px;
+  box-shadow: 5px 5px 20px #333;
+}
+.card-singlecard:nth-child(2) {
+  background: #5049b4;
+}
+.card-singlecard:nth-child(3) {
+  background: #37387c;
+}
+</style>
